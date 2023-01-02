2f394a0d448f05d0fc284d668c374b885ef41126
tarun <tarunplay3551@gmail.com>
2023-01-02 22:07:43 +0530

    search bar width

--- scripts/form-script.js
+++ scripts/form-script.js
@@ -1,8 +1,8 @@
-function test(){
+function test() {
   let uid, pw;
-  uid= document.getElementById("username").value;
-  pw= document.getElementById("password").value;
+  uid = document.getElementById("username").value;
+  pw = document.getElementById("password").value;
 
-  localStorage.setItem("user's name",uid);
-  localStorage.setItem("user's password",pw);
+  localStorage.setItem("user's name", uid);
+  localStorage.setItem("user's password", pw);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import {viteStaticCopy as copy} from 'vite-plugin-static-copy';
-import {defineConfig} from 'vite';
+import { viteStaticCopy as copy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite';
 import fs from 'fs';
 
 export default defineConfig({
   plugins: [
     copy({
-      targets: [{src: 'data/*', dest: 'data'}],
+      targets: [{ src: 'data/*', dest: 'data' }],
     }),
   ],
   base: './',
@@ -15,9 +15,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: '80',
-    https: {
-      key: fs.readFileSync('/etc/letsencrypt/live/indgeos.in/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/indgeos.in/cert.pem'),
-    },
+    // https: {
+    //   key: fs.readFileSync('/etc/letsencrypt/live/indgeos.in/privkey.pem'),
+    //   cert: fs.readFileSync('/etc/letsencrypt/live/indgeos.in/cert.pem'),
+    // },
   }
 });
